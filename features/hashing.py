461b9de7c6e51eb6a380730be794f4751ca14068
FatimaNsrn <fatimanasirian98@gmail.com>
2026-01-05 21:47:41 +0300

    Intentional Bug

--- features/hashing.py
+++ features/hashing.py
@@ -1,6 +1,8 @@
+#BROKEN CODE (Intentional Bug)
+
 from sklearn.feature_extraction import FeatureHasher
 
 
 def hash_features(X, n_features=16):
     hasher = FeatureHasher(n_features=n_features, input_type="string")
-    return hasher.transform(X.astype(str).values).toarray()
+    return hasher.transform(X.values).toarray()
